e14c8bfaeba32d4ca63440155d02fc1b87621cd1
Susan C Thomas <susancthomas@Susans-MBP.lan>
2021-02-23 00:05:59 -0500

    filter the data by datetime

--- UFO-Level-1/StarterCode/static/js/app.js
+++ UFO-Level-1/StarterCode/static/js/app.js
@@ -50,3 +50,11 @@ function runEnter() {
 
     // Select the input element and get the raw HTML node
     var inputElement = d3.select(".form-control");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    // Use the form input to filter the data by datetime
+    var filteredData = tableData.filter((sighting) => sighting.datetime === inputValue);
+    console.log(filteredData);
