9c98d9462d3536aa77c6439b3157f37f23f68c96
Susan C Thomas <susancthomas@Susans-MBP.lan>
2021-02-23 00:34:33 -0500

    commented out the instructions for the next steps and created code to print out the table

--- UFO-Level-2/StarterCode/static/js/app.js
+++ UFO-Level-2/StarterCode/static/js/app.js
@@ -1,4 +1,22 @@
+// Level 2: Multiple Search Categories (Optional)
+
+// Complete all of Level 1 criteria.
+
+// Using multiple `input` tags and/or select dropdowns, 
+// write JavaScript code so the user can to set multiple filters 
+// and search for UFO sightings using the following criteria based on the table columns:
+
+//   1. `date/time`
+//   2. `city`
+//   3. `state`
+//   4. `country`
+//   5. `shape`
+
 // from data.js
 var tableData = data;
-
 // YOUR CODE HERE!
+
+console.log(tableData);
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
