4cf3c61ebb834d81ec78695c2f65cb11d6740249
Susan C Thomas <susancthomas@Susans-MBP.lan>
2021-02-23 00:04:06 -0500

    complete the event handler functions and created code to prevent the page from reloading

--- UFO-Level-1/StarterCode/static/js/app.js
+++ UFO-Level-1/StarterCode/static/js/app.js
@@ -41,3 +41,9 @@ var form = d3.select("#form");
 // Create event handlers
 button.on("click", runEnter);
 form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
