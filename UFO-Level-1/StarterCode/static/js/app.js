28ab00e8c99b0c61ad85cb0629e39ff241a656fc
Susan C Thomas <susancthomas@Susans-MBP.lan>
2021-02-22 23:57:33 -0500

    loop through data and console.log sightings and create empty rows for table

--- UFO-Level-1/StarterCode/static/js/app.js
+++ UFO-Level-1/StarterCode/static/js/app.js
@@ -5,3 +5,18 @@ console.log(tableData);
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
+
+// Loop Through each UFO `data` and console.log each ufo-sighting object
+// Creating the empty rows for tableData
+tableData.forEach((ufoSightings) => {
+    console.log(ufoSightings);
+    // Using d3 to append one table row `tr` for each ufo-sighting object
+    var row = tbody.append("tr");
+    // Using the `Object.entries` to console.log each UFO Sighting object
+    Object.entries(ufoSightings).forEach(([key, value]) => {
+        console.log(key, value);
+        // Appending a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
